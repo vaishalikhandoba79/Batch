d636fea3eb231bf279658584120ff5a103fb8857
vaishalikhandoba79 <vaishalikhandoba@gmail.com>
2025-01-21 18:46:06 +0530

    Async

---
+++ async.js
@@ -0,0 +1,56 @@
+// console.log(10);
+// console.log(20);
+// console.log(30);
+// console.log(40);
+// console.log(50);
+
+// console.log(10);
+// console.log(20);
+// setTimeout(()=>{
+//     console.log(30); //lof
+// },5000)
+// setInterval(()=>{
+    //document.writeln("MRUH");
+//     console.lOg(30);
+// },1500)
+// console.log(40);
+// console.log(50);
+
+// let p1=new Promise((resolve,reject)=>{});
+// console.log(p1);
+// let p2=new Promise((resolve,reject)=>{
+//     resolve("Success");
+// });
+// console.log(p2);
+// let p3=new Promise((resolve,reject)=>{
+//     reject("Failure");
+
+// });
+// console.log(p3);
+
+
+// let p1 = new Promise((resolve, reject)=>{});
+// console.log(p1);
+
+//let p2 = new Promise((resolve, reject)=>{
+//     resolve("success");
+// })
+// console.log(p2);
+// p2
+// .then((response)=>console.log(response))
+// p2.
+// catch((error)=>console.log(error))
+// p2.
+// finally(()=>console.log("finally for both"))
+
+
+// let p3 = new Promise((resolve, reject)=>{
+//     reject("failure");
+// })
+// console.log(p3);
+// p3
+// .then((response)=>console.log(response))
+// p3
+// .catch((error)=>console.log(error))
+// p3
+// .finally(()=>console.log("finally for both"))
