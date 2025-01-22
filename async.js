e0ba1489f5ce4646910ae1ff6673b33f471e7df7
vaishalikhandoba79 <vaishalikhandoba@gmail.com>
2025-01-22 12:57:11 +0530

    async

--- async.js
+++ async.js
@@ -54,3 +54,31 @@
 // .catch((error)=>console.log(error))
 // p3
 // .finally(()=>console.log("finally for both"))
+
+//! API fetching (Application Programming Interface) (Backend developer-API developer)
+function fetchUsers(){
+    let response=fetch("https://jsonplaceholder.typicode.com/users");
+    //console.log(response);
+    response.then(result=>{
+        //console.log(result.json());
+        return result.json().then(data=>{
+            //console.log(data);
+            let store=document.getElementById("store");
+            console.log(store);
+            data.map(user=>{
+                //console.log(user);
+                store.innerHTML += `
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                </tr>
+                `
+            })
+        })
+    })
+    .catch(err=>console.log(err))
+}
+fetchUsers();
+
