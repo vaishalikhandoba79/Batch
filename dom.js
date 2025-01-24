36b76edd8c23b153b6ecb7417e2a6fdd60a4e03c
vaishalikhandoba79 <vaishalikhandoba@gmail.com>
2025-01-24 22:48:01 +0530

    DOM

--- dom.js
+++ dom.js
@@ -6,7 +6,74 @@
 // element.innerText = "G+"
 // console.log(element)
 
-let ele = document.getElementById("test");
+// let ele = document.getElementById("test");
 //ele.innerText= "<h1>Header<h1>"
-ele.innerHTML = "<h1>Header</h1>"
-console.log(ele);
+// ele.innerHTML = "<h1>Header</h1>"
+// console.log(ele);
+//!
+// let ele = document.getElementsByClassName("test");
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="hello"
+//ele[0].style.backgroundColor="tomato";
+
+//!SpreadOperator:- It will takeout each and every value from the original array
+//! and store it inside one more new array(pure array).
+//?Syntax:- [...variable];
+// let x = [...ele];
+//console.log(x , Array.isArray(x));
+// x.map(element=>{
+    //console.log(element);
+//     element.style.backgroundColor="orange"
+// })
+//let ele = document.getElementByName("formData");
+// console.log(ele, Array.isArray(ele));
+//[...ele].map(inp.value);
+//})
+
+//!
+// let ele = document.querySelector("#mru");
+// console.log(ele);
+
+// let ele = document.querySelector(".test");
+// console.log(ele);
+
+// let ele = document.querySelectorAll(".test");
+// console.log(ele);
+
+//let btn = document.querySelector("#btn");
+//! addEventListener("event" ,()=>{})
+    // btn.addEventListener("click",()=>{
+    //     console.log("Button Clicked");
+    //     alert("hi")
+    //     document.body.style.backgroundColor="tomato"
+    // })
+
+// let btn = document.querySelector("#btn");
+// btn.addEventListener("dblcli",()=>{
+//     console.log("double clicked");
+// })
+
+// let divBlock = document.getElementById("divBlock");
+// divBlock.addEventListener("mouseover",()=>{
+//     document.body.style.backgroundColor="yellow"
+//     divBlock.style.backgroundColor="blue"
+// })
+
+// divBlock.addEventListener("mouseover",()=>{
+//     document.body.style.backgroundColor="orange"
+//     divBlock.style.backgroundColor="aqua"
+// })
+
+let inp = document.getElementById("inp");
+inp.addEventListener("keydown",()=>{
+    console.log("keydown");
+})
+
+
+
+
+
+
+
+
