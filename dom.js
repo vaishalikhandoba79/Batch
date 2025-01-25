0c077f8ce813f5c11c7695ec5f9a392f35841a3b
vaishalikhandoba79 <vaishalikhandoba@gmail.com>
2025-01-25 12:55:49 +0530

    DOM

--- dom.js
+++ dom.js
@@ -65,15 +65,53 @@
 //     divBlock.style.backgroundColor="aqua"
 // })
 
-let inp = document.getElementById("inp");
-inp.addEventListener("keydown",()=>{
-    console.log("keydown");
+// let inp = document.getElementById("inp");
+// inp.addEventListener("keydown",()=>{
+//     console.log("keydown");
+// })
+
+
+// let bgColor = document.querySelectorAll(".bgColor");
+     //console.log(bgColor);
+// [...bgColor].map((element)=>{
+         //console.log(element);
+//     element.addEventListener("mouseover",()=>{
+         //console.log(element.innerText);
+//         element.computedStyleMap.backgroundColor=element.innerText;
+
+//     });
+
+// })
+
+// let ele = document.createElement("hi");
+// ele.innerText = "Dynamic creation of HTML element";
+// ele.setAttribute("id" , "demo");
+// ele.id = "demo";
+//ele.removeAttribute("id")
+//console.log(ele);
+
+// let image = document.createElement("img");
+// image.src = "./";
+// console.log(image);
+// document.body.appendChild(ele);
+// document.body.appendChild(image);
+
+
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let mail = document.getElementById("uMail");
+let psw = document.getElementById("uPass");
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    // console.log(event);
+    // console.log("form submitted");
+    let uname = username.value;
+    let uemail = mail.value;
+    let upass = psw.value;
+    let userDetails={
+        uname , uemail, upass
+    }
+    console.log(userDetails);
+    localStorage.setItem("userData", JSON.stringify(userDetails))
 })
-
-
-
-
-
-
-
 
