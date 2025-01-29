8e9955ab7204aff32acb371a7617e0871bac24f2
vaishalikhandoba79 <vaishalikhandoba@gmail.com>
2025-01-29 13:02:49 +0530

    Task

---
+++ task1.js
@@ -0,0 +1,16 @@
+// ! Take a array of employee object where each object contains properties such as EMP ID, 
+// ! EMP Name, EMP Company, EMP salary, & EMP address where EMP address is further having two 
+// ! more properties of object such as EMP city, EMP Area print these properties in the form 
+//! of the table on the UI by using javascript.
+
+let userDetails = {
+    EmpId: 1,
+    Empname: "Vaishali",
+    EmpCompany: "Infosys",
+    EmpSalary: "100000",
+    EmpAddress:{
+        EmpCity: "Armoor",
+        EmpArea: "Nizambad",
+    }
+};
+console.log(userDetails);
